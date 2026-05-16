65c3bfedbf9f789b2ed99f4eb8164632d52cbc2f
Zachary Louie <zacharylouie@Zacharys-MacBook.local>
2026-05-15 22:43:19 -0700

    add bike lanes in Boston and Cambridge

--- map.js
+++ map.js
@@ -11,3 +11,35 @@ const map = new mapboxgl.Map({
   minZoom: 5,
   maxZoom: 18,
 });
+
+map.on("load", async () => {
+  const bikeLaneStyle = {
+    "line-color": "#32D400",
+    "line-width": 5,
+    "line-opacity": 0.6,
+  };
+
+  map.addSource("boston_route", {
+    type: "geojson",
+    data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson",
+  });
+
+  map.addLayer({
+    id: "boston-bike-lanes",
+    type: "line",
+    source: "boston_route",
+    paint: bikeLaneStyle,
+  });
+
+  map.addSource("cambridge_route", {
+    type: "geojson",
+    data: "https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson",
+  });
+
+  map.addLayer({
+    id: "cambridge-bike-lanes",
+    type: "line",
+    source: "cambridge_route",
+    paint: bikeLaneStyle,
+  });
+});
